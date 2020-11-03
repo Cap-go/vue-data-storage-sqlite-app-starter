c9655c7772f53f7fc96329f50f95933302e9504f
jepiqueau <jepi.queau@free.fr>
2020-11-03 18:44:02 +0100

    fix android import

--- android/app/src/main/java/com/jeep/app/vue/hook/datastoragesqlite/MainActivity.java
+++ android/app/src/main/java/com/jeep/app/vue/hook/datastoragesqlite/MainActivity.java
@@ -6,6 +6,8 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
+import com.jeep.plugin.capacitor.CapacitorDataStorageSqlite;
+
 
 public class MainActivity extends BridgeActivity {
   @Override
@@ -16,6 +18,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+      add(CapacitorDataStorageSqlite.class);
     }});
   }
 }
